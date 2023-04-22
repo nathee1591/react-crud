2b4241dfe5072a122ce3aba9b47cc87971dbfbbe
Natee Muenchananan <natee.tee159@gmail.com>
2023-04-23 00:48:07 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes , Route} from "react-router-dom";
 
 import Navbar from './Navbar'
 import Users from "./Users";
+import UserCreate from "./UserCreate";
 import UserUpdate from "./UserUpdate";
-import Usercreate from "./components/Usercreate"
 
 export default function App() {
   return (
@@ -12,9 +12,9 @@ export default function App() {
       <Navbar />
       <Routes>
           <Route path='/' element={<Users />} />
-          {/* <Route path='/create' element={<UserCreate />} /> */}
+          <Route path='/create' element={<UserCreate />} />
           <Route path='/update/:id' element={<UserUpdate />} />
-        </Routes>
+      </Routes>
     </div>
   );
 }
